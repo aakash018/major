2c79a27528fbc7d0849851bd92ee9268fd84380a
aakash018 <aakashkhanal015@gmail.com>
2024-02-08 15:32:20 +0545

    pwa added

--- web/src/pages/plants.tsx
+++ web/src/pages/plants.tsx
@@ -1,15 +1,7 @@
-import MobileNav from "@/components/custom/MobileNav";
-import NavBar from "@/components/custom/NavBar";
 import PageTheme from "@/components/custom/PageTheme";
 import ProtectedRoutes from "@/components/custom/ProtectedRoute";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import img from "../assets/home.jpg";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+
 import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
 import axiosInstance from "@/axiosInstance";
@@ -17,15 +9,6 @@ import { PlantsType, ServerResponse } from "@/types/global";
 import { toast } from "sonner";
 import { useNavigate } from "react-router-dom";
 import { CircleNotch, WarningCircle } from "@phosphor-icons/react";
-
-const tomatoPlants = [
-  { name: "Cherry Delight", plantingDate: "15/02/2023" },
-  { name: "Sunset Bliss", plantingDate: "08/04/2023" },
-  { name: "Royal Red", plantingDate: "23/06/2023" },
-  { name: "Emerald Dream", plantingDate: "10/01/2023" },
-  { name: "Golden Harvest", plantingDate: "05/09/2023" },
-  // Add more tomato plants as needed
-];
 
 const Plants = () => {
   const [plants, setPlants] = useState<PlantsType[]>([]);
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,10 +1,53 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 
 import path from "path";
 
+const manifestForPlugIn: Partial<VitePWAOptions> = {
+  registerType: "prompt",
+  includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "React-vite-app",
+    short_name: "react-vite-app",
+    description: "I am a simple vite app",
+    icons: [
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+      {
+        src: "/maskable_icon.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+    theme_color: "#171717",
+    background_color: "#f0e7db",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugIn)],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
